4664385fb3c2d5b7fc26b5c78b5d6d64ecc325d3
pranaychennoju <pranay.c@cogentibs.in>
2024-09-05 09:13:42 +0530

    firstcommit

---
+++ lib/dashboard/mylearnings_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MylearningsScreen extends StatelessWidget {
+  const MylearningsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('My learnings'),
+      ),
+    );
+  }
+}
